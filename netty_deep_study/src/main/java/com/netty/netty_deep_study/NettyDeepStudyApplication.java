6b439f1d74d6c7c4ff0960c5d4e1668b7e9152c3
zsp <15396226187@163.com>
2021-08-26 08:38:58 +0800

    着手netty的深入学习，先复习一下网络编程

---
+++ netty_deep_study/src/main/java/com/netty/netty_deep_study/NettyDeepStudyApplication.java
@@ -0,0 +1,13 @@
+package com.netty.netty_deep_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyDeepStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyDeepStudyApplication.class, args);
+    }
+
+}
